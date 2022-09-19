2c4cd0668c5eae7c574d36dc8a55b5641327394a
Lydia Yuan <lydiayuan99@gmail.com>
2022-09-19 15:21:02 -0600

    add: separated structure for VS Code & Xcode dev

---
+++ build/CMakeFiles/finalProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finalProject.dir/src/main.cpp.o"
+  "CMakeFiles/finalProject.dir/src/main.cpp.o.d"
+  "finalProject"
+  "finalProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/finalProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for finalProject.
+# This may be replaced when dependencies are built.
